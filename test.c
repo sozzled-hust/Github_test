4605e84b037940491bfcdd21137d047ca5fad7c6
sozzled-hust <758311971@qq.com>
2021-11-18 21:47:44 +0800

    test

--- test.c
+++ test.c
@@ -4,8 +4,8 @@ int main(void)
 	printf("helloworld\n");
 	printf("test1\n");
 	printf("hotfix!\n");
-	printf("test1!\n");
 	printf("only for test1 branch\n");
   printf("sozzled\n");
+  printf("dddd\n");
 	return 0;
 }
